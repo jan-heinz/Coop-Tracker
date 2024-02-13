eef08bc34423b48f80bef811bb6c8547bf760553
winnoran <anwinnor@gmail.com>
2024-02-13 18:04:13 -0500

    Added ejs file
    Added an ejs file for the profile page to hold coops that have been applied to.

--- api/controllers/controllers.js
+++ api/controllers/controllers.js
@@ -2,23 +2,47 @@ import path from "path";
 
 class PageController {
     static getHomepage(req, res) {
-        res.sendFile(path.resolve() + "/index.html");
+        res.sendFile(path.resolve() + "/public/html/index.html");
     }
 
     static getLogin(req, res) {
-        res.sendFile(path.resolve() + "/login.html");
+        res.sendFile(path.resolve() + "/public/html/login.html");
     }
 
     static getProfile(req, res) {
-        res.sendFile(path.resolve() + "/profile.html");
+
+        const coop1 = {
+            company: "NUSci",
+            location: "Boston",
+            date_applied: "1/24/2024",
+            app_status: "Accepted",
+        }    
+
+        const coop2 = {
+            company: "Oasis",
+            location: "Boston",
+            date_applied: "1/15/2024",
+            app_status: "Declined",
+        }   
+
+        const coop3 = {
+            company: "Amazon",
+            location: "Seattle",
+            date_applied: "1/10/2024",
+            app_status: "Ghosted",
+        } 
+
+        const coop = [coop1, coop2, coop3];
+
+        res.render("profile", { coop });
     }
 
     static getAdd(req, res) {
-        res.sendFile(path.resolve() + "/add.html");
+        res.sendFile(path.resolve() + "/public/html/add.html");
     }
 
     static getFriends(req, res) {
-        res.sendFile(path.resolve() + "/friends.html");
+        res.sendFile(path.resolve() + "/public/html/friends.html");
     }
 }
 
